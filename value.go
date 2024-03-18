cf148d34d89ed2e2f56d91320925722296c3ee4f
beanjs <445212619@qq.com>
2024-03-18 14:11:51 +0800

    oops

--- value.go
+++ value.go
@@ -1,7 +1,6 @@
 package value
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"strconv"
@@ -12,61 +11,56 @@ func Get(v interface{}, name string) (interface{}, error) {
 	ns := strings.Split(name, ".")
 	nv := reflect.ValueOf(v)
 
-	switch nv.Kind() {
-	case reflect.Struct, reflect.Map, reflect.Array, reflect.Slice, reflect.Ptr:
-		for idx, n := range ns {
-			knd := nv.Kind()
+	for idx, n := range ns {
+		knd := nv.Kind()
 
-			if knd == reflect.Ptr {
-				nv = reflect.Indirect(nv)
-				knd = nv.Kind()
-			}
-
-			if !nv.IsValid() {
-				return nil, fmt.Errorf("invalid field: %s", n)
-			}
-
-			switch knd {
-			case reflect.Struct:
-				nv = nv.FieldByName(n)
-				if nv.Kind() == reflect.Invalid {
-					return nil, fmt.Errorf("invalid field: %s", n)
-				}
-			case reflect.Array, reflect.Slice:
-				i, err := strconv.Atoi(n)
-				if err != nil {
-					return nil, err
-				}
-				nv = nv.Index(i)
-			case reflect.Map:
-				fd := false
-
-				for _, k := range nv.MapKeys() {
-					if fmt.Sprint(k.Interface()) == n {
-						nv = nv.MapIndex(k)
-						fd = true
-						break
-					}
-				}
-
-				if !fd {
-					return nil, fmt.Errorf("invalid field: %s", n)
-				}
-			default:
-				return nil, fmt.Errorf("invalid field: %s, should be struct,map,array", ns[idx-1])
-			}
+		if knd == reflect.Ptr {
+			nv = reflect.Indirect(nv)
+			knd = nv.Kind()
 		}
 
 		if !nv.IsValid() {
-			return nil, fmt.Errorf("invalid field: %s", name)
-		} else if nv.CanInterface() {
-			return nv.Interface(), nil
+			return nil, fmt.Errorf("invalid field: %s", n)
 		}
 
-		return nil, fmt.Errorf("unexported field: %s", name)
-	default:
-		return nil, errors.New("invalid src, should be struct,map,array")
+		switch knd {
+		case reflect.Struct:
+			nv = nv.FieldByName(n)
+			if nv.Kind() == reflect.Invalid {
+				return nil, fmt.Errorf("invalid field: %s", n)
+			}
+		case reflect.Array, reflect.Slice:
+			i, err := strconv.Atoi(n)
+			if err != nil {
+				return nil, err
+			}
+			nv = nv.Index(i)
+		case reflect.Map:
+			fd := false
+
+			for _, k := range nv.MapKeys() {
+				if fmt.Sprint(k.Interface()) == n {
+					nv = nv.MapIndex(k)
+					fd = true
+					break
+				}
+			}
+
+			if !fd {
+				return nil, fmt.Errorf("invalid field: %s", n)
+			}
+		default:
+			return nil, fmt.Errorf("invalid field: %s, should be struct,map,array", ns[idx-1])
+		}
 	}
+
+	if !nv.IsValid() {
+		return nil, fmt.Errorf("invalid field: %s", name)
+	} else if nv.CanInterface() {
+		return nv.Interface(), nil
+	}
+
+	return nil, fmt.Errorf("unexported field: %s", name)
 }
 
 func MustGet(v interface{}, name string) interface{} {
